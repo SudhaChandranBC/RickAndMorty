8c6fae1ab00dd718a58f61fc02656c8b35f63316
Chandran, Sudha <sudha.chandran@rakuten.com>
2021-06-18 17:32:09 +0530

    Added test cases

---
+++ RickAndMortyTests/ModelTests.swift
@@ -0,0 +1,68 @@
+//
+//  ViewModelTests.swift
+//  RickAndMortyTests
+//
+//  Created by Chandran, Sudha on 18/06/21.
+//
+
+import XCTest
+@testable import RickAndMorty
+import Combine
+
+class ModelTests: XCTestCase {
+    
+    func testSearchResponse() throws {
+        guard let path = Bundle(for: type(of: self)).path(forResource: "AllEpisodes", ofType: "json") else {
+            fatalError("AllEpisodes.json not found")
+        }
+        let data = try Data(contentsOf: URL(fileURLWithPath: path))
+        let response = try JSONDecoder().decode(EpisodeInfoModel.self, from: data)
+        
+        XCTAssertEqual(response.info.count, 41)
+        XCTAssertEqual(response.info.pages, 3)
+        XCTAssertEqual(response.info.next!, "https://rickandmortyapi.com/api/episode?page=2")
+        XCTAssertNil(response.info.prev)
+        XCTAssertTrue((response.results.count != 0))
+        
+        let firstEpisode = response.results.first!
+        
+        XCTAssertEqual(firstEpisode.id, 1)
+        XCTAssertEqual(firstEpisode.name, "Pilot")
+        XCTAssertEqual(firstEpisode.airDate, "December 2, 2013")
+        XCTAssertEqual(firstEpisode.episode, "S01E01")
+        XCTAssertEqual(firstEpisode.characters.count, 19)
+        XCTAssertEqual(firstEpisode.seasonNumber(), "Season 1")
+        XCTAssertEqual(firstEpisode.episodeNumber(), "Episode 1")
+    }
+    
+    func testCharacterViewModel() throws {
+        guard let path = Bundle(for: type(of: self)).path(forResource: "AllCharacters", ofType: "json") else {
+            fatalError("AllCharacters.json not found")
+        }
+        let data = try Data(contentsOf: URL(fileURLWithPath: path))
+        let response = try JSONDecoder().decode(CharacterInfoModel.self, from: data)
+        
+        XCTAssertEqual(response.info.count, 671)
+        XCTAssertEqual(response.info.pages, 34)
+        XCTAssertEqual(response.info.next!, "https://rickandmortyapi.com/api/character?page=2")
+        XCTAssertNil(response.info.prev)
+        XCTAssertTrue((response.results.count != 0))
+        
+        let viewModel = CharacterViewModel()
+        viewModel.characters = response.results
+        
+        let firstCharacter = viewModel.characters.first!
+        
+        XCTAssertEqual(viewModel.characters.count, 20)
+        XCTAssertEqual(firstCharacter.name, "Rick Sanchez")
+        XCTAssertEqual(firstCharacter.species, "Human")
+        XCTAssertEqual(firstCharacter.imageURL(), URL(string: firstCharacter.image))
+        
+        let expectation = XCTestExpectation(description: "Test for ImageLoader")
+        let loader = ImageLoader.init(url: firstCharacter.imageURL())
+        expectation.fulfill()
+        loader.load()
+    }
+    
+}
+
--- RickAndMortyTests/RickAndMortyTests.swift
+++ RickAndMortyTests/RickAndMortyTests.swift
@@ -7,27 +7,88 @@
 
 import XCTest
 @testable import RickAndMorty
+import Combine
 
 class RickAndMortyTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var networkHandler = NetworkHandler()
+    let apiClient = RickMortyAPI()
+    var cancellable: AnyCancellable?
+    
+    func testRequestAllEpisodes() {
+        
+        let expectation = XCTestExpectation(description: "Request all episodes")
+        
+        cancellable = apiClient.getAllEpisodes()
+            .sink(receiveCompletion: { _ in }, receiveValue: { episodes in
+                episodes.forEach() { print ($0.name) }
+                expectation.fulfill()
+            })
+        wait(for: [expectation], timeout: 10.0)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testRequestAllCharacters() {
+        
+        let expectation = XCTestExpectation(description: "Request all characters")
+        
+        cancellable = apiClient.getAllCharacters()
+            .sink(receiveCompletion: { _ in }, receiveValue: { characters in
+                characters.forEach() { print ($0.name) }
+                expectation.fulfill()
+            })
+        
+        wait(for: [expectation], timeout: 10.0)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testNetworkRequestByMethodError() {
+        
+        let expectation = XCTestExpectation(description: "Test for error handling in request by method")
+        
+        networkHandler.performAPIRequestByMethod(method: "abcd") {
+            switch $0 {
+            case .success( _):
+                break
+            case.failure(let error):
+                print(error)
+                expectation.fulfill()
+            }
         }
     }
-
+    
+    func testJSONResponseDataParsing() {
+        
+        let expectation = XCTestExpectation(description: "Test decoding data response")
+        
+        networkHandler.performAPIRequestByMethod(method: "character/1") {
+            switch $0 {
+            case .success(let data):
+                if let decodedData: CharacterModel = self.networkHandler.decodeJSONData(data: data) {
+                    print(decodedData.name)
+                    expectation.fulfill()
+                }
+            case.failure( _):
+                break
+            }
+        }
+        wait(for: [expectation], timeout: 10.0)
+    }
+    
+    func testJSONResponseDataParsingError() {
+        
+        let expectation = XCTestExpectation(description: "Test decoding data response")
+        
+        networkHandler.performAPIRequestByMethod(method: "character/1") {
+            switch $0 {
+            case .success(let data):
+                if let _: CharacterInfoModel = self.networkHandler.decodeJSONData(data: data) {
+                } else {
+                    print("decoding failed")
+                    expectation.fulfill()
+                }
+            case.failure( _):
+                break
+            }
+        }
+        wait(for: [expectation], timeout: 10.0)
+    }
 }
